a45b61997c219019da1116669a8ed9c620ad74f5
naeem4633 <ahmedn3700@gmail.com>
2023-08-20 05:49:54 +0500

    Minor change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,9 +8,9 @@ const Home = () => {
     <Header navigation={false} transparent={true}/>
     <section className="w-full h-[100vh] bg-gray-950 flex items-center text-gray-300">
       <div className='h-[70vh] lg:h-3/4 w-full flex flex-col lg:flex-row justify-around items-center lg:items-end mt-20'>
-        <div className='lg:h-3/4 flex flex-col space-y-4 justify-center lg:justify-end tracking-wider font-semibold lg:mb-32'>
+        <div className='lg:h-3/4 flex flex-col space-y-2 md:space-y-4 justify-center lg:justify-end tracking-wider font-semibold lg:mb-32'>
           <p>SYED NAEEM AHMED</p>
-          <p className='font-light'>Full Stack Developer / Web Designer</p>
+          <p className='font-light text-sm md:text-base'>Website Developer</p>
         </div>
         <div className='h-2/3 lg:h-full flex flex-col w-full lg:w-2/5 font-light text-7xl lg:text-[10rem] 2xl:text-[12rem] items-center'>
           <div className='h-full flex flex-col space-y-10 lg:space-y-0'>
